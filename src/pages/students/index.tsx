6b3471d93eca0da14e160580cc56080d925352cb
Yuno Fujita <tsujimuralabgithub@gmail.com>
2024-02-19 11:17:40 +0900

    Upload image by  YF

--- src/pages/students/index.tsx
+++ src/pages/students/index.tsx
@@ -4,8 +4,11 @@ import { css } from '@linaria/core'
 import { useTranslation } from 'next-export-i18n'
 
 import { ArticleContainer } from '@/components/template/ArticleContainer'
-import { Text } from '@/components/typography/Text'
 import { Title } from '@/components/typography/Title'
+
+import { Heading } from '@/components/typography/Heading'
+import { basePath } from '@/utils/basePath'
+
 
 export default function ForStudents() {
   const { t } = useTranslation()
@@ -17,8 +20,16 @@ export default function ForStudents() {
 
       <ArticleContainer>
         <Title>{t('students.heading')}</Title>
-        <section className={section}>
-          <Text>{t('students.content')}</Text>
+        
+
+        
+        <section>
+          <Heading>{t('students.content.colorScience.details')}</Heading>
+          <div className={imageBlock}>
+            <img src={`${basePath}${t("students.content.colorScience.picture")}`} />
+          </div>
+        
+          
         </section>
       </ArticleContainer>
     </>
@@ -27,4 +38,14 @@ export default function ForStudents() {
 
 const section = css`
   height: calc(100svh - 12rem - 304px);
+`
+
+const imageBlock = css`
+  width: 100%;
+
+  img {
+    width: 100%;
+    aspect-ratio: 3 / 2;
+    object-fit: cover;
+  }
 `
